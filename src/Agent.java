8d42c17882aab645d0529c53f566bd96d6491d6f
Basile <ngy.b@hotmail.com>
2021-04-16 17:28:07 -0400

    écriture succinte de l'algorithme

--- src/Agent.java
+++ src/Agent.java
@@ -8,8 +8,29 @@ public class Agent {
 
     }
 
+    //Pour dog1
     public void algoMiniMax()
     {
+//      action = tourMax(n)
+    }
 
+    public void tourMax(Noeud n)
+    {
+//       vérifier qu'un état final n'est pas atteint sinon on retourne l'utilité
+
+//       on prends l'ensemble des actions possibles de dog1
+//            on récupére laction avec la plus haute utilité retourné par TourMin
+
+//       return actionChoisie la plus profitable a dog1
+    }
+
+    public void tourMin(Noeud n)
+    {
+//       vérifier qu'un état final n'est pas atteint sinon on retourne l'utilité
+
+//       on prends l'ensemble des actions possibles de dog2
+//            on récupére laction avec la plus faible utilité retourné par TourMax
+
+//       return actionChoisie la plus profitable a dog1
     }
 }
